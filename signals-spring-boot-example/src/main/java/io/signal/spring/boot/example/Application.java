863c8622ba3b7123d2f11683bcbb21052958fb96
Maksym Maksymchuk <mmaksymchuk@eastbanctech.com>
2019-12-13 16:52:34 -0500

    Added example of runtime retuning

--- signals-spring-boot-example/src/main/java/io/signal/spring/boot/example/Application.java
+++ signals-spring-boot-example/src/main/java/io/signal/spring/boot/example/Application.java
@@ -1,6 +1,7 @@
 package io.signal.spring.boot.example;
 
 import io.signal.Signal;
+import io.signal.SignalReceiver;
 import io.signal.spring.boot.example.transmitter.FirstChannelTransmitter;
 import io.signal.spring.boot.example.transmitter.SecondChannelTransmitter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +22,17 @@ public class Application implements CommandLineRunner {
     @Autowired
     private SecondChannelTransmitter secondChannelTransmitter;
 
+    @Autowired
+    private SignalReceiver<CustomMessage> customMessageReceiver;
+
     @Override
     public void run(String... args) {
         firstChannelTransmitter.transmit(Signal.message("First Hello World!"));
         firstChannelTransmitter.transmit(Signal.message(new CustomMessage()));
         secondChannelTransmitter.transmit(Signal.message("Second Hello World!"));
+
+        customMessageReceiver.tune(secondChannelTransmitter.getChannel());
+        secondChannelTransmitter.transmit(Signal.message(new CustomMessage()));
     }
 
     public static void main(String[] args) {
